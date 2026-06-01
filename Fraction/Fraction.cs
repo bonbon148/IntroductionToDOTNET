8ead8e82d6aff93007bccfc684c37013afc74857
bonbon148 <townpony608@gmail.com>
2026-06-01 11:31:32 +0300

    Fraction Constructors DONE

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    internal class Fraction
+    {
+        public int Integer { get; set; }    //Автоствойсто, описывающее целую часть
+        public int Numerator { get; set; }  //Автоствойсто, описывающее числитель 
+        int denominator;    //Переменная, описывающая знаменатель
+        public int Denominator  //Свойство, описывающее знаменатель 
+        {
+            get => denominator; //Если метод можно реализвоать одним выражением,
+                                //то его можно написать без фигурных скобок,
+                                //вместо этого, после круглых скобок ставится => (скаречнэк)
+            set => denominator = value == 0 ? 1 : value;
+        }
+
+        //              Constructors:
+        public Fraction()
+        {
+            this.Integer = 0;
+            this.Numerator = 0;
+            this.Denominator = 1;
+            Console.WriteLine($"DefaultConstructor:{this.GetHashCode()}");
+        }
+        public Fraction(int integer)
+        {
+            this.Integer = integer;
+            this.Numerator = 0;
+            this.Denominator = 1;
+            Console.WriteLine($"Single-ArgumentConstructor:{GetHashCode()}");            
+        }
+        public Fraction(int numerator, int denominator)
+        {
+            this.Integer = 0;
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+            Console.WriteLine($"Constructor:\t{GetHashCode()}");
+        }
+        public Fraction(int integer, int numerator, int denominator)
+        {
+            this.Integer = integer;
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+            Console.WriteLine($"Constructor:\t{GetHashCode()}");
+        }
+        //                Methods:
+        public void Print()
+        {
+
+            if (Integer != 0) Console.Write(Integer);
+            if (Numerator != 0)
+            {
+                if (Integer != 0) Console.Write("(");
+                Console.Write($"{Numerator}/{Denominator}");
+                if (Integer != 0) Console.Write(")");
+            }
+            else if (Integer == 0) Console.Write(0);
+            Console.WriteLine();
+                    
+        }
+    }
+}
---
+++ Fraction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Fraction A = new Fraction();
+            A.Print();
+
+            Fraction B = new Fraction(5);
+            B.Print();
+
+            Fraction C = new Fraction(1, 2);
+            C.Print();
+
+            Fraction D = new Fraction(2, 3, 4);
+            D.Print();
+        }
+    }
+}
